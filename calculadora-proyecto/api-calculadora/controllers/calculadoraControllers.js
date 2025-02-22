f8a120884ed1ac2f93b6d937c7c9e2c4582e1b4a
edwinbravo34 <edwinbravo2121@gmail.com>
2025-02-22 17:42:55 -0500

    agregar boton de dividir

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,34 +1,43 @@
-const {add, subtract, multiply} = require('../operaciones/operaciones.js');
+const { add, subtract, multiply, divide } = require('../operaciones/operaciones.js');
 
-function sumar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function sumar(req, res) {
+    const { body } = req;
+    const { number1, number2 } = body;
     const result = add(number1, number2);
     res.json({
         resultado: result
     });
 }
 
-function restar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function restar(req, res) {
+    const { body } = req;
+    const { number1, number2 } = body;
     const result = subtract(number1, number2);
     res.json({
         resultado: result
     })
 }
 
-function multiplicar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function multiplicar(req, res) {
+    const { body } = req;
+    const { number1, number2 } = body;
     const result = multiply(number1, number2);
     res.json({
         resultado: result
     })
 }
 
+function dividir(req, res) {
+    const { body } = req;
+    const { number1, number2 } = body;
+    const result = divide(number1, number2);
+    res.json({
+        resultado: result
+    })
+}
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    dividir
 }
--- calculadora-proyecto/api-calculadora/operaciones/operaciones.js
+++ calculadora-proyecto/api-calculadora/operaciones/operaciones.js
@@ -5,26 +5,38 @@
  * @param {Number} b 
  * @returns Number
  */
-function add(a, b){
+function add(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 + number2;
 }
 
-function subtract(a, b){
+function subtract(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 - number2;
 }
 
-function multiply(a, b){
+function multiply(a, b) {
     let number1 = parseInt(a);
     let number2 = parseInt(b);
     return number1 * number2;
 }
 
+function divide(a, b) {
+    let number1 = parseInt(a);
+    let number2 = parseInt(b);
+
+    if (number2 === 0) {
+        return "Error: No se puede dividir por cero";
+    }
+
+    return number1 / number2;
+}
+
 module.exports = {
     add,
     subtract,
-    multiply
+    multiply,
+    divide
 }
