35b480c60babde89f6dc92307c1500369ee214d8
ErickSanchez2003 <erick.sanchez01@unicatolica.edu.co>
2025-03-04 18:44:08 -0500

    boton de division

--- calculadora-proyecto-20250222T221642Z-001/calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
+++ calculadora-proyecto-20250222T221642Z-001/calculadora-proyecto/api-calculadora/routes/calculadora.routes.js
@@ -6,5 +6,6 @@ router
     .post('/sumar', calculadoraControllers.sumar)
     .post('/restar', calculadoraControllers.restar)
     .post('/multiplicar', calculadoraControllers.multiplicar)
+    .post('/division', calculadoraControllers.division)
 
 module.exports = router;
