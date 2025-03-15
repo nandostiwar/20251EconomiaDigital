60a410adb6af4d6e8ef656157f4d93051744f1bb
Esteban-Lombar <esteban.castano01@unicatolica.edu.co>
2025-03-15 01:32:38 -0500

    Se junta la base de datos

--- pedidos/backend/server.js
+++ pedidos/backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // Conexión a MongoDB
-mongoose.connect('mongodb+srv://pedro:AMRrq2AQFEIbibOJ@cluster0.cjuezls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+mongoose.connect('mongodb+srv://estebancastano01:xH0G03GGAdOez3tv@cluster2025.ndydq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2025', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
