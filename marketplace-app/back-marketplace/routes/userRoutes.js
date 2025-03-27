a5d77240d60a77c68f33107654f85788d7b679e2
edwinbravo34 <edwinbravo2121@gmail.com>
2025-03-27 15:04:29 -0500

    borrar marketplace

--- marketplace-app/back-marketplace/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controllers/userController');
-const router = express.Router();
-
-router.post('/newUser', controller.newUser);
-router.post('/newAdmin', controller.newAdmin);
-router.post('/login', controller.login);
-
-module.exports = router;
